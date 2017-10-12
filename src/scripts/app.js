71b2d7c5e57cfcd4fd6f2eb16f893cba907bd6c8
Alex Klymenko <klymenko.sasha21@gmail.com>
2017-10-12 17:21:27 +0300

    add parallax effect on scroll

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,7 @@
 const slider = require('./common/slider');
 const $ = require('jquery'); // если будет нужен
 const burgerMenu = require('./common/burgerMenu');
+const parallax = require('./common/parallax');
 
 const scrollOnBlog = require('./common/scrollOnBlog');
 
